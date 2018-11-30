24bf4019728884b0c33e69a75c6c348f5cf9b89d
Filip Wästberg <filip.wastberg@ferrologic.se>
2018-11-30 16:13:17 +0100

    wpr

--- wp.R
+++ wp.R
@@ -1,10 +1,10 @@
 # Publish to wordpress
 
 devtools::install_github(c("duncantl/XMLRPC", "duncantl/RWordPress"))
-
+options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
 library(RWordPress)
-options(WordpressLogin = c(user = "filip", password = 'lV%OHj!Gg%A!yk*$TnlLJEhK'),
-        WordpressURL = 'https://www.ferrologic.se/wp-admin/edit.php')
+options(WordpressLogin = c(user = 'filip&lV%OHj!Gg%A!yk*$TnlLJEhK'),
+        WordpressURL = 'https://www.ferrologic.se/wp-admin/xmlrpc.phpp')
 library(knitr)
 knit2wp('ordnaalys.Rmd', title = 'Your post title', action = c("newPost"),
         encoding = getOption("encoding"), publish = FALSE)
